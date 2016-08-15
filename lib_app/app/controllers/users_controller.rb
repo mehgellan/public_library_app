0f975664d20a70d60d412b0ec4cd36c46276c63a
mehgellan <baumegg@gmail.com>
2016-08-15 16:12:21 -0700

    successful auth--first run

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -9,17 +9,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
     @user = User.create(user_params)
     redirect_to root_path
   end
 
   def show
     @user = User.find_by_id(params[:id])
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root to: "users#index"
+
   get "/users" => "users#index", as: "users"
-
   get "/users/new" => "users#new", as: "new_user"
-
   post "/users" => "users#create"
+  get "/users/:id" => "users#show", as: "user"
 
-  get "/users/:id" => "users#show"
+  get "/login" => "sessions#new"
+  get "/logout" => "sessions#destroy"
+  post "/sessions" => "sessions#create"
 end
