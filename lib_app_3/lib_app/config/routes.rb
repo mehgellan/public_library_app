061fc179aed56a81e651743a437f6f091ba9a90e
mehgellan <baumegg@gmail.com>
2016-08-15 21:24:34 -0700

    third run--in the middle of create new session

---
+++ lib_app_3/lib_app/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root "users#index"
+  get "/users" => "users#index", as: "users"
+  get "/users/new" => "users#new", as: "new_user"
+  post "/users" => "users#create"
+  get "/users/:id" => "users#show", as: "user"
+
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+end
